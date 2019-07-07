481529558cd0238694e09208ac61df6988d7865b
Richter Brzeski <rmbrzeski@gmail.com>
2019-07-07 15:17:37 -0400

    Update User-Agent

--- examples/edit_request.py
+++ examples/edit_request.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     sess = pycharles.session.CharlesSession(path=CHARLES_SESSION_PATH)
     print('session has {} requests.'.format(sess.requests_count()))
     print('changing user-agent of the first request...')
-    sess.query_request_with_index(0).set_header('user-agent', 'google chrome')
+    sess.query_request_with_index(0).set_header('User-Agent', 'Nokia 8110')
     sess.save('../example_sessions/edited_httpbin_request.chlsj')
